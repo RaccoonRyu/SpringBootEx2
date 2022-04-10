43540d8f510bb07e2adf68fa22b2095b1b26a874
RaccoonRyu <rjhwind94@gmail.com>
2022-04-10 16:53:14 +0900

    초안반영(Spring web, devtools, lombok, thymeleaf) 커밋

---
+++ src/test/java/org/zerock/exboot2/Exboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.exboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exboot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
